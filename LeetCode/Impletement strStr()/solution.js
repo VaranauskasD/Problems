13666ccfd24129deba44acba76d34adc25856251
Varnas <dvaranauskas7@gmail.com>
2022-01-22 11:34:53 +0000

    added: LeetCode dir and sample str problem

---
+++ LeetCode/Impletement strStr()/solution.js
@@ -0,0 +1,2 @@
+// Return the index of the first occurrence of a string in a string. Return 0 if the string is empty or -1 if it does not occur.
+const strStr = (haystack, needle) => (needle ? haystack.indexOf(needle) : 0)
