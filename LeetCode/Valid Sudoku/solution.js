84c9fdcd0ca7cd1d038e23cb96748f180b80e8e4
varanauskasd <dvaranauskas7@gmail.com>
2022-04-25 01:26:50 +0300

    fix: valid sudoku

--- LeetCode/Valid Sudoku/solution.js
+++ LeetCode/Valid Sudoku/solution.js
@@ -1,22 +1,50 @@
-// Map array to a hash containing quantities, translate to a tuple array, sort based on the quantity
+// Time complexity would not change, thus not optimised validation
 
-const topKFrequent = (nums, k) => {
-  const obj = {}
-  nums.forEach((num) => {
-    if (!obj[num]) obj[num] = 0
-    obj[num]++
-  })
+const isValidSudoku = (board) => {
+  let flagArray = new Array(9).fill(0)
+  let valid = true
 
-  const temp = []
-  for (key in obj) {
-    temp.push([key, obj[key]])
+  for (let row = 0; row < 9 && valid; row++) {
+    for (let col = 0; col < 9 && valid; col++) {
+      if (board[row][col] !== '.') {
+        flagArray[`${board[row][col] - 1}`]++
+      }
+      if (flagArray[`${board[row][col] - 1}`] > 1) {
+        valid = false
+      }
+    }
+    flagArray = new Array(9).fill(0)
+  }
+  if (!valid) return valid
+
+  for (let col = 0; col < 9 && valid; col++) {
+    for (let row = 0; row < 9 && valid; row++) {
+      if (board[row][col] !== '.') {
+        flagArray[`${board[row][col] - 1}`]++
+      }
+      if (flagArray[`${board[row][col] - 1}`] > 1) {
+        valid = false
+      }
+    }
+    flagArray = new Array(9).fill(0)
+  }
+  if (!valid) return valid
+
+  for (let i = 0; i < 3 && valid; i++) {
+    for (let k = 0; k < 3 && valid; k++) {
+      for (let row = i * 3; row < 3 * (i + 1) && valid; row++) {
+        for (let col = k * 3; col < 3 * (k + 1) && valid; col++) {
+          if (board[row][col] !== '.') {
+            flagArray[`${board[row][col] - 1}`]++
+          }
+          if (flagArray[`${board[row][col] - 1}`] > 1) {
+            valid = false
+          }
+        }
+      }
+      flagArray = new Array(9).fill(0)
+    }
   }
 
-  temp.sort((a, b) => b[1] - a[1])
-
-  const res = []
-  for (let i = 0; i < k; i++) {
-    res.push(temp[i][0])
-  }
-  return res
+  return valid
 }
