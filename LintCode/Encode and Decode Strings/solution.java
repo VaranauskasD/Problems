7976e706505146744d32e98f02a001842a194287
varanauskasd <dvaranauskas7@gmail.com>
2022-04-25 02:20:55 +0300

    fix: encode decode solution in java

--- LintCode/Encode and Decode Strings/solution.java
+++ LintCode/Encode and Decode Strings/solution.java
@@ -1,50 +1,20 @@
-// Time complexity would not change, thus not optimised validation
+// Encode by adding a suffix. Decode using a split with the suffix param.
 
-const isValidSudoku = (board) => {
-  let flagArray = new Array(9).fill(0)
-  let valid = true
-
-  for (let row = 0; row < 9 && valid; row++) {
-    for (let col = 0; col < 9 && valid; col++) {
-      if (board[row][col] !== '.') {
-        flagArray[`${board[row][col] - 1}`]++
+public class Solution {
+  public String encode(List<String> strs) {
+      String encoded = "";
+      for (int i = 0; i < strs.size(); i++) {
+          encoded += strs.get(i);
+          if (i != strs.size() - 1) encoded += ":;";
       }
-      if (flagArray[`${board[row][col] - 1}`] > 1) {
-        valid = false
-      }
-    }
-    flagArray = new Array(9).fill(0)
-  }
-  if (!valid) return valid
-
-  for (let col = 0; col < 9 && valid; col++) {
-    for (let row = 0; row < 9 && valid; row++) {
-      if (board[row][col] !== '.') {
-        flagArray[`${board[row][col] - 1}`]++
-      }
-      if (flagArray[`${board[row][col] - 1}`] > 1) {
-        valid = false
-      }
-    }
-    flagArray = new Array(9).fill(0)
-  }
-  if (!valid) return valid
-
-  for (let i = 0; i < 3 && valid; i++) {
-    for (let k = 0; k < 3 && valid; k++) {
-      for (let row = i * 3; row < 3 * (i + 1) && valid; row++) {
-        for (let col = k * 3; col < 3 * (k + 1) && valid; col++) {
-          if (board[row][col] !== '.') {
-            flagArray[`${board[row][col] - 1}`]++
-          }
-          if (flagArray[`${board[row][col] - 1}`] > 1) {
-            valid = false
-          }
-        }
-      }
-      flagArray = new Array(9).fill(0)
-    }
+      return encoded;
   }
 
-  return valid
+  public List<String> decode(String str) {
+      List<String> decoded = new ArrayList<String>();
+      for (String s : str.split(":;")) {
+          decoded.add(s);
+      }
+      return decoded;
+  }
 }
