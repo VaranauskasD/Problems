1f9d4c1bb074bea9d0fe5cfd2749dc94b28ef871
varanauskasd <dvaranauskas7@gmail.com>
2022-05-02 22:45:36 +0100

    wip: climbing stairs

--- LeetCode/Climbing Stairs/solution.js
+++ LeetCode/Climbing Stairs/solution.js
@@ -2,9 +2,10 @@
 // as the bottom steps' branches' totals can be used to calculate the total of all the posibilities
 
 const climbStairs = (n) => {
-  ;(step_current = 0), (step_next = 1)
+  let step_current = 0,
+    step_next = 1
   for (let i = 0; i < n; i++) {
-    temp = step_next
+    let temp = step_next
     step_next = step_next + step_current
     step_current = temp
   }
