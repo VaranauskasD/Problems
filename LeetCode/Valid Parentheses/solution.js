b72b9fb06b854313238f52666de03e293fe2d9d8
Varnas <dvaranauskas7@gmail.com>
2022-05-05 23:37:41 +0100

    fix: valid parentheses

--- LeetCode/Valid Parentheses/solution.js
+++ LeetCode/Valid Parentheses/solution.js
@@ -1,1 +1,21 @@
-const validParentheses = (s) => {}
+// Maintain a stack to check whether the last item in the stack matches
+// the current character using a parentheses map
+
+const isValid = (s) => {
+  const obj = { ')': '(', ']': '[', '}': '{' }
+  const stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    if (!obj.hasOwnProperty(s[i])) {
+      stack.push(s[i])
+      console.log(stack)
+    } else {
+      if (stack.length < 1 || stack[stack.length - 1] !== obj[s[i]]) {
+        return false
+      }
+      stack.pop()
+    }
+  }
+
+  return stack.length < 1
+}
