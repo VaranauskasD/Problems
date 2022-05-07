6a2836dbba8bc4ca34f6ec67df5aa6b308c75d39
Varnas <dvaranauskas7@gmail.com>
2022-05-07 23:05:06 +0100

    wip: climbing stairs

--- LeetCode/Climbing Stairs/solution.js
+++ LeetCode/Climbing Stairs/solution.js
@@ -1,5 +1,6 @@
 // Using DP the problem breaks down to a fibonnachi sequence,
 // as the bottom steps' branches' totals can be used to calculate the total of all the posibilities
+// wip
 
 const climbStairs = (n) => {
   let step_current = 0,
