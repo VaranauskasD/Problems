a62e720f9c954020f13fa520e083fca08c35072e
Varnas <dvaranauskas7@gmail.com>
2022-05-09 23:48:40 +0100

    wip: LRUCache

---
+++ LeetCode/LRU Cache/solution.js
@@ -0,0 +1,18 @@
+var LRUCache = function (capacity) {
+  this.capacity = capacity
+  this.cacheKey = null
+  this.cacheObj = {}
+}
+
+LRUCache.prototype.get = function (key) {
+  return this.cacheObj[key] ? this.cacheObj[key] : -1
+}
+
+LRUCache.prototype.put = function (key, value) {
+  if (Object.keys(this.cacheObj).length >= this.capacity) {
+    delete this.cacheObj[this.cacheKey]
+  }
+  this.cacheKey = key
+  this.cacheObj[key] = value
+  return
+}
